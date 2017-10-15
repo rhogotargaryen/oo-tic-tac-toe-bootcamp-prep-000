e0abe92ff6be64e8dd7228e71343b928b20af3cb
Derek Haller <dhaller.personal@gmail.com>
2017-10-15 03:35:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class TicTacToe
     index
   end
   def move(index, current_player = "X")
-    @board[index] = current_player()
+    @board[index] = current_player
   end
   def position_taken?(index)
     if ((@board[index] == "X") || (@board[index] == "O"))
