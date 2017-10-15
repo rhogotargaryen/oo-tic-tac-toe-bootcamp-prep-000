52e5280e941cb2062588e4e1a4b2e825f05b00f0
Derek Haller <dhaller.personal@gmail.com>
2017-10-15 03:30:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,8 +64,8 @@ class TicTacToe
     puts "please chose a number 1 - 9:"
     input = gets.strip
     index = input_to_index(input)
-    current_player = cplay
-    if valid_move?(index, cplay)
+    current_player
+    if valid_move?(index)
       move(index)
     else
       turn
